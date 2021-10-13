4e0068ba2598f199f1f53bb5a0f735b668ef2d19
iprafnow <iprafnow@IP-NB59>
2021-10-13 11:24:17 +0200

    Add project files.

---
+++ BookStore-API/Mappings/Maps.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Mappings
+{
+    public class Maps : Profile
+    {
+
+    }
+}
